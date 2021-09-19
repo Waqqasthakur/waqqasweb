8efc794d048ce6a689aa7d0b9b760d62b57ac730
Thakur Waqqas <waqqasthakur@gmail.com>
2021-09-19 23:56:36 +0530

    Updated on 19.09.2021

---
+++ main.js
@@ -0,0 +1,14 @@
+// const projectClick = document.getElementById('projectClick');
+// const closeButton = document.getElementById('closeButton');
+// projectClick.addEventListener('click', (e) => {
+//     const v = document.getElementById("projectSpace");
+//     v.classList.add("project-space-expanded");
+
+// })
+// closeButton.addEventListener('click', (e) => {
+//     const v = document.getElementById("projectSpace");
+//     v.classList.remove("project-space-expanded");
+//     v.style.height = "220px";
+//     projectClick.inn = false;
+
+// })
